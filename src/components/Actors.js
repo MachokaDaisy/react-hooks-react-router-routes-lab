3eeb84d695ed2cc624b3aa0d3f93b68d96f62b3f
Machoka Daisy <machokadaisy25@gmail.com>
2022-08-02 15:28:49 +0300

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { actors } from "../data";
 
-function Actors() {
-  return <div>{/*{code here}*/}</div>;
-}
+const Actors = () => {
+  const actorItems = actors.map((item) => (
+    <div key={item.name}>
+      <h2>{item.name}</h2>
+      <ul>
+        {item.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorItems}
+    </div>
+  );
+};
 
 export default Actors;
